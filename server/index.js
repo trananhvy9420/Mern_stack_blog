15d4b88529b507e50dbf683ec5b9f8eb4e4441b7
trananhvy9420 <trananhvy9420@gmail.com>
2025-11-28 12:34:35 +0700

    feat: update server setup with Express, add dotenv for environment variables, and configure .gitignore

--- server/index.js
+++ server/index.js
@@ -1,1 +1,17 @@
-    
+import express from "express";
+import bodyParser from "body-parser";
+import cors from "cors";
+import dotenv from "dotenv";
+import mongoose from "mongoose";
+
+dotenv.config();
+
+const app = express();
+const PORT = process.env.PORT || 5000;
+app.use(bodyParser.json({ limit: "30mb" }));
+app.use(bodyParser.urlencoded({ limit: "30mb", extended: true }));
+app.use(cors());
+app.get("/", (req, res) => {
+  res.send("Hello World!");
+});
+app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
